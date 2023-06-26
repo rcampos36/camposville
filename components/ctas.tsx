b3321c8e12469f52a6dacab7af0efce6aef18aa6
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-26 14:53:01 -0400

    adding new cta content

--- components/ctas.tsx
+++ components/ctas.tsx
@@ -5,15 +5,15 @@ export default function Ctas() {
     <div className='bg-[color:var(--third-green-color)] py-28 w-full'>
       <div className='flex gap-4 w-[1280px] mx-auto justify-between'>
         <div className='w-1/3 relative flex items-center justify-center'>
-          <img className='z-0' src='/public/images/cta.png' alt='cta' />
+          <img className='z-0' src='./images/cta.png' alt='cta' />
           <button className=' absolute z-10 w-[200px] h-[50px] bg-white px-4 py-3 rounded-lg shadow-lg hover:shadow-xl'>Organic Juice</button>
         </div>
         <div className='w-1/3 relative flex items-center justify-center'>
-          <img className='z-0' src='/public/images/cta2.png' alt='cta' />
+          <img className='z-0' src='./images/cta2.png' alt='cta' />
           <button className='absolute z-10 w-[200px] h-[50px] bg-white px-4 py-3 rounded-lg shadow-lg hover:shadow-xl'>Organic Foods</button>
         </div>
         <div className='w-1/3 relative flex items-center justify-center'>
-          <img className='z-0' src='/public/images/cta3.png' alt='cta' />
+          <img className='z-0' src='./images/cta3.png' alt='cta' />
           <button className='absolute z-10 w-[200px] h-[50px] bg-white px-4 py-3 rounded-lg shadow-lg hover:shadow-xl'>Nuts and Cookies</button>
         </div>
       </div>
