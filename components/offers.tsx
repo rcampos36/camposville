9fe5a4bd7cf4e6ee4088d35a9449351e4ef31e5e
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-26 15:00:12 -0400

    adding new ctacards content

--- components/offers.tsx
+++ components/offers.tsx
@@ -9,19 +9,19 @@ export default function Offers() {
             <h4 className={`${roboto.className} font-bold text-3xl`}>What We Offer For You</h4>
             <div className='flex gap-4'>
                 <div className='w-1/4 flex flex-col items-center gap-2'>
-                    <img src='/images/spices.png' alt='offers' />
+                    <img src='./images/spices.png' alt='offers' />
                     <p>Spices</p>
                 </div>
                 <div className='w-1/4 flex flex-col items-center gap-2'>
-                    <img src='/images/seeds.png' alt='offers' />
+                    <img src='./images/seeds.png' alt='offers' />
                     <p>Seeds</p>
                 </div>
                 <div className='w-1/4 flex flex-col items-center gap-2'>
-                    <img src='/images/fruits.png' alt='offers' />
+                    <img src='./images/fruits.png' alt='offers' />
                     <p>Fruits</p>
                 </div>
                 <div className='w-1/4 flex flex-col items-center gap-2'>
-                    <img src='/images/vegetables.png' alt='offers' />
+                    <img src='./images/vegetables.png' alt='offers' />
                     <p>Vegetables</p>
                 </div>
             </div>
