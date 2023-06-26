0165101ef0a3898f71ab2833906253e85494a117
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-26 16:06:46 -0400

    adding new src image content

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,12 @@
 import NewsCards from "@/components/news-cards";
 import { yellowtail, roboto } from "../../components/styles/fonts";
+import NewsBanner from "@/public/static/images/NewsBanner.png";
 
 export default function News() {
   return (
     <div>
       <div className="w-full relative">
-        <img src="/images/NewsBanner.png" alt="about banner" />
+        <img src={NewsBanner.src} alt="about banner" />
         <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
           <h1 className={`${roboto.className} font-bold text-6xl`}>Recent News</h1>
         </div>
