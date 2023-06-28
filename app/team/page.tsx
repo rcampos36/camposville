6cebfe3a89311f7514edfe7bbc84b6593f47c529
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-28 10:52:07 -0400

    adding lazy loading content

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,12 +2,13 @@ import TeamBanner from "@/public/static/images/Team-Banner.jpg";
 import { yellowtail, roboto } from "../../components/styles/fonts";
 import OurTeam from "@/components/our-team";
 import Newsletter from "@/components/newsletter";
+import Image from "next/image";
 
 export default function Team() {
   return (
     <div className="">
       <div className="w-full relative">
-        <img src={TeamBanner.src} alt="about banner" />
+        <Image src={TeamBanner} alt="about banner" />
         <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
           <h1 className={`${roboto.className} font-bold text-6xl`}>Our Team</h1>
         </div>
