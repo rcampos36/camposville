58d1f523acb66dd26ce855ed1d3e474ec1206257
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-29 12:39:58 -0400

    adding mobile styles

--- components/organic-only.tsx
+++ components/organic-only.tsx
@@ -7,11 +7,11 @@ export default function OrganicOnly() {
   return (
     <div className='relative w-full pb-20'>
         <Image src={Organic} alt='organic' />
-        <div className='absolute top-0 left-0 w-full mx-auto flex flex-col items-center gap-3 py-20'>
+        <div className='absolute top-0 left-0 w-full mx-auto flex flex-col items-center gap-3 py-20 max-sm:py-4'>
             <p className={`${yellowtail.className} yellowtail text-2xl font-normal`}>Organic Only</p>
-            <h4 className={`${roboto.className} font-bold text-4xl`}>Everyday Fresh & Clean</h4>
-            <p className="text-sm text-gray-400 w-1/2 text-center">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the </p>
-            <p className='text-6xl mt-10'><FaPlayCircle className='text-[color:var(--primary-green-color)]'/></p>
+            <h4 className={`${roboto.className} font-bold text-4xl max-sm:text-3xl`}>Everyday Fresh & Clean</h4>
+            <p className="text-sm text-gray-400 max-sm:text-gray-600 w-1/2 max-sm:w-full text-center">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the </p>
+            <p className='text-6xl max-sm:text-4xl max-sm:mt-3 mt-10'><FaPlayCircle className='text-[color:var(--primary-green-color)]'/></p>
         </div>
     </div>
   )
--- components/what-we-grow.tsx
+++ components/what-we-grow.tsx
@@ -12,34 +12,34 @@ import BagImage from "@/public/static/images/bag-image.png";
 
 export default function WhatWeGrow() {
   return (
-    <div className="w-[1080px] mx-auto my-20">
+    <div className="w-[1080px] mx-auto my-20 max-sm:w-full px-4">
         <div className="flex flex-col items-center justify-center">
             <p className={`${yellowtail.className} yellowtail text-2xl font-normal`}>What we grow</p>
-            <h4 className={`${roboto.className} font-bold text-4xl w-2/5 text-center`}>Better Agriculture for Better Future</h4>
+            <h4 className={`${roboto.className} font-bold text-4xl w-2/5 max-sm:w-full max-sm:mb-10 text-center`}>Better Agriculture for Better Future</h4>
         </div>
-        <div className='flex gap-5 items-center'>
-            <div className='flex flex-col gap-2 w-1/3'>
-                <div className='flex flex-col justify-end items-end text-right gap-1'>
+        <div className='flex gap-5 items-center max-sm:flex-col'>
+            <div className='flex flex-col gap-2 w-1/3 max-sm:w-full'>
+                <div className='flex flex-col justify-end items-end text-right gap-1 max-sm:text-left max-sm:items-start'>
                     <Image className='w-[40px]' src={Dairy} alt='dairy' />
                     <h4 className={`${roboto.className} font-bold text-sm`}>Dairy Products</h4>
                     <p className='text-xs text-gray-400'>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
                 </div>
-                <div className='flex flex-col justify-end items-end text-right gap-1'>
+                <div className='flex flex-col justify-end items-end text-right gap-1 max-sm:text-left max-sm:items-start'>
                     <Image className='w-[40px]' src={Store} alt='store' />
                     <h4 className={`${roboto.className} font-bold text-sm`}>Store Services</h4>
                     <p className='text-xs text-gray-400'>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
                 </div>
-                <div className='flex flex-col justify-end items-end text-right gap-1'>
+                <div className='flex flex-col justify-end items-end text-right gap-1 max-sm:text-left max-sm:items-start'>
                     <Image className='w-[40px]' src={Delivery} alt='delivery' />
                     <h4 className={`${roboto.className} font-bold text-sm`}>Delivery Services</h4>
                     <p className='text-xs text-gray-400'>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
                 </div>
             </div>
-            <div className='w-1/2'>
+            <div className='w-1/2 max-sm:w-full'>
                 <Image src={BagImage} alt='what we grow' />
             </div>
-            <div className='flex flex-col gap-2 w-1/3'>
-            <div className='flex flex-col justify-start items-start text-left gap-1'>
+            <div className='flex flex-col gap-2 w-1/3 max-sm:w-full max-sm:mb-5'>
+                <div className='flex flex-col justify-start items-start text-left gap-1'>
                     <Image className='w-[40px]' src={Agriculture} alt='agriculture' />
                     <h4 className={`${roboto.className} font-bold text-sm`}>Agriculture</h4>
                     <p className='text-xs text-gray-400'>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
