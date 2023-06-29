5ca06cc70f2d61b3cafa4f7b559e9b28af4bf53e
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-29 12:54:30 -0400

    adding mobile styles

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,7 +10,7 @@ export default function News() {
       <div className="w-full relative">
         <Image src={NewsBanner} alt="about banner" />
         <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
-          <h1 className={`${roboto.className} font-bold text-6xl`}>Recent News</h1>
+          <h1 className={`${roboto.className} font-bold text-6xl max-sm:text-4xl`}>Recent News</h1>
         </div>
       </div>
       <NewsCards />
