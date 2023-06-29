0c5d653094f7f82a65398dd860a776ddd3fe8932
roger campos <macuser@Macs-MacBook-Pro.local>
2023-06-29 12:02:09 -0400

    adding mobile styles

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -10,7 +10,7 @@ export default function Team() {
       <div className="w-full relative">
         <Image src={TeamBanner} alt="about banner" />
         <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
-          <h1 className={`${roboto.className} font-bold text-6xl`}>Our Team</h1>
+          <h1 className={`${roboto.className} font-bold text-6xl max-sm:text-4xl`}>Our Team</h1>
         </div>
       </div>
       <OurTeam />
--- components/our-team.tsx
+++ components/our-team.tsx
@@ -11,12 +11,12 @@ import Image from 'next/image';
 export default function OurTeam() {
   return (
     <div className='mt-20'>
-        <div className='w-[1080px] flex flex-col gap-2 items-center mx-auto'>
+        <div className='w-[1080px] flex flex-col gap-2 items-center mx-auto max-sm:w-full max-sm:px-4'>
             <p className={`${yellowtail.className} yellowtail text-2xl font-normal`}>Team</p>
             <h4 className={`${roboto.className} font-bold text-4xl`}>Our Organic Experts</h4>
             <p className='text-gray-400 text-center text-sm w-2/3'>Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
-            <div className='flex gap-2 justify-between mt-5'>
-                <div className='w-1/3' >
+            <div className='flex gap-2 justify-between mt-5 max-sm:flex-col max-sm:gap-5'>
+                <div className='w-1/3 max-sm:w-full' >
                     <Image src={TeamOne} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Giovani Bacardo</h4>
@@ -31,7 +31,7 @@ export default function OurTeam() {
                         
                     </div>
                 </div>
-                <div className='w-1/3'>
+                <div className='w-1/3 max-sm:w-full'>
                     <Image src={TeamTwo} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Marianne Loreno</h4>
@@ -46,7 +46,7 @@ export default function OurTeam() {
                         
                     </div>
                 </div>
-                <div className='w-1/3'>
+                <div className='w-1/3 max-sm:w-full'>
                     <Image src={TeamThree} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Riga Pelore</h4>
@@ -62,8 +62,8 @@ export default function OurTeam() {
                     </div>
                 </div>  
             </div>
-            <div className='flex gap-2 justify-between mt-5'>
-                <div className='w-1/3' >
+            <div className='flex gap-2 justify-between mt-5 max-sm:flex-col max-sm:gap-5'>
+                <div className='w-1/3 max-sm:w-full' >
                     <Image src={TeamFour} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Keira Knightley</h4>
@@ -78,7 +78,7 @@ export default function OurTeam() {
                         
                     </div>
                 </div>
-                <div className='w-1/3'>
+                <div className='w-1/3 max-sm:w-full'>
                     <Image src={TeamFive} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Scott Lawrence</h4>
@@ -93,7 +93,7 @@ export default function OurTeam() {
                         
                     </div>
                 </div>
-                <div className='w-1/3'>
+                <div className='w-1/3 max-sm:w-full'>
                     <Image src={TeamSix} alt='team' />
                     <div className='p-5 bg-[color:var(--secondary-gray-color)] hover:bg-white hover:shadow-xl rounded-br-2xl rounded-bl-3xl'>
                         <h4 className={`${roboto.className} font-bold text-sm`}>Karen Allen</h4>
